bf4962d3e53f0230217fffea7ab1a7a87c944df8
MrYawe <yannis.weishaupt@gmail.com>
2016-12-02 05:10:17 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!([
-    {email: 'yannis_weishaupt@hotmail.fr', name: 'Asso1', password: '123456'},
-    {email: 'germain_clauss@yahoo.fr', name: 'Asso2', password: '123456'},
-    {email: 'melchior.vila@laposte.net', name: 'Asso3', password: '123456'},
-    {email: 's.arnaud67@hotmail.fr', name: 'Asso4', password: '123456'}
+    {email: 'user1@hotmail.fr', name: 'Asso1', password: '123456'},
+    {email: 'user2@yahoo.fr', name: 'Asso2', password: '123456'},
+    {email: 'user3@laposte.net', name: 'Asso3', password: '123456'},
+    {email: 'user4@hotmail.fr', name: 'Asso4', password: '123456'}
 ])
 
 Event.create!([
     {
         title: "title 1", description: 'description 1', adress: "22 rue de rien", city: "Strasbourg",
-        country: "France", date: Time.now, event_type: 2, latitude: 48.5688834,
+        country: "France", date: Time.now, event_type: 3, latitude: 48.5688834,
         longitude: 7.7545109, user: User.first
     },
     {
@@ -26,12 +26,12 @@ Event.create!([
     },
     {
         title: "title 2", description: 'description 2', adress: "22 rue de rien", city: "Strasbourg",
-        country: "France", date: Time.now, event_type: 2, latitude: 50.5688834,
+        country: "France", date: Time.now, event_type: 5, latitude: 50.5688834,
         longitude: 4.7545109, user: User.second
     },
     {
         title: "title 3", description: 'description 2', adress: "22 rue de rien", city: "Strasbourg",
-        country: "France", date: Time.now, event_type: 2, latitude: 49.5688834,
+        country: "France", date: Time.now, event_type: 1, latitude: 49.5688834,
         longitude: 5.7545109, user: User.second
     },
 ])

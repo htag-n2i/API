d2ca59eef7d14e76e58875a33bc2b0a099052079
MrYawe <yannis.weishaupt@gmail.com>
2016-12-02 04:37:42 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!([
+    {email: 'yannis_weishaupt@hotmail.fr', name: 'Asso1', password: '123456'},
+    {email: 'germain_clauss@yahoo.fr', name: 'Asso2', password: '123456'},
+    {email: 'melchior.vila@laposte.net', name: 'Asso3', password: '123456'},
+    {email: 's.arnaud67@hotmail.fr', name: 'Asso4', password: '123456'}
+])
+
+Event.create!([
+    {
+        title: "title 1", description: 'description 1', adress: "22 rue de rien", city: "Strasbourg",
+        country: "France", date: Time.now, event_type: 2, latitude: 48.5688834,
+        longitude: 7.7545109, user: User.first
+    },
+    {
+        title: "title 2", description: 'description 2', adress: "22 rue de rien", city: "Strasbourg",
+        country: "France", date: Time.now, event_type: 2, latitude: 47.5688834,
+        longitude: 7.7545109, user: User.first
+    },
+    {
+        title: "title 2", description: 'description 2', adress: "22 rue de rien", city: "Strasbourg",
+        country: "France", date: Time.now, event_type: 2, latitude: 50.5688834,
+        longitude: 4.7545109, user: User.second
+    },
+    {
+        title: "title 3", description: 'description 2', adress: "22 rue de rien", city: "Strasbourg",
+        country: "France", date: Time.now, event_type: 2, latitude: 49.5688834,
+        longitude: 5.7545109, user: User.second
+    },
+])

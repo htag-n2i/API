29666100d5bfde84b80b2ffd6c1ae0cecdd460d7
MrYawe <yannis.weishaupt@gmail.com>
2016-12-02 07:58:48 +0100

    ok

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,12 @@ class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    if current_user
+        redirect_to '/home'
+    end
+    super
+  end
 
   # POST /resource/sign_in
   # def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "landing#index"
+  get 'home', :to => 'home#index'
+
 
   devise_for :users, controllers: { registrations: 'users/registrations'  }
-
-  get 'test', :to => 'home#test'
 end
